b925748c502057a4dc0f0785bbecefbc9d4a303d
Nolen Johnson <johnsonnolen@gmail.com>
2018-11-27 20:42:52 +0000

    b4s4: Compile kernel in-line
    * Define need for DTBO image and remove prebuilt logic.
    * No need for prebuilt kernel/kernel modules logic as we'll never use it.
    * We use Clang built kernel.
    
    Change-Id: I9483a46c294cbc1edf594cf5ec77556d36e20735

--- BoardConfig-gahs.mk
+++ BoardConfig-gahs.mk
@@ -14,7 +14,24 @@
 # limitations under the License.
 #
 
-# vendor.img
+# Common board config for bonito, sargo
+
+# Build vendor image
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 AB_OTA_PARTITIONS += vendor
+
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+
+BOARD_KERNEL_IMAGE_NAME := Image.lz4
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_KERNEL_ADDITIONAL_FLAGS := \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CONFIG := dank_defconfig
+TARGET_KERNEL_SOURCE := kernel/google/b4s4
 DISABLE_ARTIFACT_PATH_REQUIREMENTS := true
+
+-include vendor/google/bonito/BoardConfigVendor.mk
