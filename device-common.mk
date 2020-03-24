1b41ac21963aa76420cd5dbd78b042663d76afbc
stebomurkn420 <stevn4127@gmail.com>
2020-03-24 04:30:24 -0400

    VoLTE and Permissions Hack

--- device-common.mk
+++ device-common.mk
@@ -129,3 +129,22 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 export BLISS_BUILDTYPE=OFFICIAL
 export BLISS_DEVELOPER=stebomurkn420
+
+# VoLTE
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.radio.rat_on=combine \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1
+
+# Permission HACKERY
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.control_privapp_permissions=log \
+    ro.adb.secure=0 \
+    persist.sys.usb.config=mtp,adb \
+    persist.service.debuggable=1 \
+    persist.service.adb.enable=1
